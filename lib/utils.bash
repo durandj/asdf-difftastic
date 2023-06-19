6fdc72f60198dc4b560ded0056190b8ac5759a36
James Durand <james@durand.zone>
2023-06-19 10:50:54 -0500

    Updated to not use template values anymore

--- lib/utils.bash
+++ lib/utils.bash
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+set -o errexit -o nounset -o pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for difftastic.
 GH_REPO="https://github.com/Wilfred/difftastic"
 TOOL_NAME="difftastic"
 TOOL_TEST="difftastic --help"
@@ -25,7 +24,7 @@ sort_versions() {
 }
 
 list_github_tags() {
-	git ls-remote --tags --refs "$GH_REPO" |
+	git ls-remote --tags --refs "${GH_REPO}" |
 		grep -o 'refs/tags/.*' | cut -d/ -f3- |
 		sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
 }
@@ -36,39 +35,58 @@ list_all_versions() {
 	list_github_tags
 }
 
+release_file_name() {
+	local tool_name="$1"; shift
+	local version="$1"; shift
+
+	case "$(uname -o)" in
+	Darwin) echo "${tool_name}-${version}-x86_64-apple-darwin.tar.gz" ;;
+	GNU/Linux) echo "${tool_name}-${version}-x86_64-unknown-linux-gnu.tar.gz" ;;
+	*)
+		echo "Unsupported operating system: '$(uname -o)'" >&2
+		exit 1
+		;;
+	esac
+}
+
 download_release() {
 	local version filename url
-	version="$1"
-	filename="$2"
+	version="$1"; shift
+	filename="$1"; shift
 
-	# TODO: Adapt the release URL convention for difftastic
-	url="$GH_REPO/archive/v${version}.tar.gz"
+	case "$(uname -o)" in
+	Darwin) url="${GH_REPO}/releases/download/${version}/difft-x86_64-apple-darwin.tar.gz" ;;
+	GNU/Linux) url="${GH_REPO}/releases/download/${version}/difft-x86_64-unknown-linux-gnu.tar.gz" ;;
+	*)
+		echo "Unsupported operating system: '$(uname -o)'" >&2
+		exit 1
+		;;
+	esac
 
-	echo "* Downloading $TOOL_NAME release $version..."
-	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
+	echo "* Downloading ${TOOL_NAME} release ${version}..."
+	curl "${curl_opts[@]}" -o "${filename}" -C - "${url}" || fail "Could not download ${url}"
 }
 
 install_version() {
-	local install_type="$1"
-	local version="$2"
-	local install_path="${3%/bin}/bin"
+	local install_type="$1"; shift
+	local version="$1"; shift
+	local install_path="${1%/bin}/bin"; shift
 
-	if [ "$install_type" != "version" ]; then
-		fail "asdf-$TOOL_NAME supports release installs only"
+	if [ "${install_type}" != "version" ]; then
+		fail "asdf-${TOOL_NAME} supports release installs only"
 	fi
 
 	(
-		mkdir -p "$install_path"
-		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
+		mkdir -p "${install_path}"
+		cp -r "${ASDF_DOWNLOAD_PATH}"/* "${install_path}"
 
-		# TODO: Assert difftastic executable exists.
 		local tool_cmd
-		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
-		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
+		tool_cmd="$(echo "${TOOL_TEST}" | cut -d' ' -f1)"
+		test -x "${install_path}/${tool_cmd}" || fail "Expected ${install_path}/${tool_cmd} to be executable."
 
-		echo "$TOOL_NAME $version installation was successful!"
+		echo "${TOOL_NAME} ${version} installation was successful!"
 	) || (
-		rm -rf "$install_path"
-		fail "An error occurred while installing $TOOL_NAME $version."
+		rm -rf "${install_path}"
+		fail "An error occurred while installing ${TOOL_NAME} ${version}."
 	)
 }
